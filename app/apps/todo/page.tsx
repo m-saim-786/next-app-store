e791138b66c87f4d1b90f1814e3d6651b9c9cda4
Muhammad Saim <m.saim.senior@gmail.com>
2023-10-11 09:48:47 -0700

    Fix server side rendering

--- app/apps/todo/page.tsx
+++ app/apps/todo/page.tsx
@@ -1,13 +1,12 @@
 import TodoList from "@/components/todo/TodoList";
-import { prisma } from "@/lib/prisma";
 
-export default async function Todo() {
-  const todoList = await prisma.todo.findMany()
+export default async function Page() {
+  const { todos } = await fetch(`${process.env.BASE_URL}/api/todo`).then(res => res.json());
 
   return (
     <main className="mx-auto w-1/2">
       <h1 className="text-3xl">Todo App</h1>
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todos} />
     </main>
   );
 }
